a9ecf9b7cb1078620ee8d2ae03cf60b593216eb3
Daniel Sears <Daniel.Sears@gmail.com>
2019-10-12 00:03:50 -0700

    removed unnecessary imports

--- books/api/views.py
+++ books/api/views.py
@@ -1,10 +1,4 @@
-from    rest_framework.permissions    import   (AllowAny,
-                                                IsAuthenticated,
-                                                IsAdminUser,
-                                                IsAuthenticatedOrReadOnly,
-                                                DjangoModelPermissions,
-                                                DjangoModelPermissionsOrAnonReadOnly,
-                                                DjangoObjectPermissions)
+from    rest_framework.permissions    import    DjangoModelPermissions, DjangoObjectPermissions
 from    rest_framework.generics       import   (CreateAPIView,
                                                 ListAPIView,
                                                 RetrieveAPIView,
