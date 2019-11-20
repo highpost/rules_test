4eb1e950795182a81d1fb5f87fa975f49a6ffd6b
Daniel Sears <Daniel.Sears@gmail.com>
2019-11-20 11:38:45 -0800

    changed BookSerializer and RBookSerializer from ModelSerializer to the more explicit Serializer superclass

--- books/api/serializers.py
+++ books/api/serializers.py
@@ -1,29 +1,45 @@
-from    rest_framework.serializers    import    ModelSerializer, SlugRelatedField
+from    rest_framework.serializers    import    CharField, SlugRelatedField, Serializer
 from    django.contrib.auth           import    get_user_model
 from    ..models                      import    Book, RBook
 
 
-class BookSerializer(ModelSerializer):
+class BookSerializer(Serializer):
+    title   = CharField(max_length = 100)
+    isbn    = CharField(max_length = 50)
     author  = SlugRelatedField(
+                slug_field  = 'username',
                 queryset    = get_user_model().objects.all(),
                 many        = False,
-                read_only   = False,
-                slug_field  = 'username'
+                read_only   = False
              )
 
-    class Meta:
-        model   = Book
-        fields  = '__all__'
+    def create(self, validated_data):
+        return Book.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title   = validated_data.get('title',  instance.title)
+        instance.isbn    = validated_data.get('isbn',   instance.isbn)
+        instance.author  = validated_data.get('author', instance.author)
+        instance.save()
+        return instance
 
 
-class RBookSerializer(ModelSerializer):
+class RBookSerializer(Serializer):
+    title   = CharField(max_length = 100)
+    isbn    = CharField(max_length = 50)
     author  = SlugRelatedField(
+                slug_field  = 'username',
                 queryset    = get_user_model().objects.all(),
                 many        = False,
-                read_only   = False,
-                slug_field  = 'username'
+                read_only   = False
              )
 
-    class Meta:
-        model   = RBook
-        fields  = '__all__'
+    def create(self, validated_data):
+        return RBook.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title   = validated_data.get('title',  instance.title)
+        instance.isbn    = validated_data.get('isbn',   instance.isbn)
+        instance.author  = validated_data.get('author', instance.author)
+        instance.save()
+        return instance
